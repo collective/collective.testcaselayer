69b0032887a949db81f79d62cc0e0b3245a9cae5
rossp <rossp@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-19 01:34:57 +0000

    * Initial paster layout with some setup mods

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.testcaselayer',
+      version=version,
+      description="Use various test case classes as zope.testing layers",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Ross Patterson',
+      author_email='me@rpatterson.net',
+      url='http://pypi.python.org/pypi/collective.testcaselayer',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'zope.testing',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
