9764b88b5c8d5db71b889ab3ef06093bda51f9ce
rossp <rossp@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-01-29 01:31:52 +0000

    Create a common testing layer with useful debugging stuff

---
+++ src/collective/testcaselayer/common.py
@@ -0,0 +1,24 @@
+from collective.testcaselayer import ptc
+from collective.testcaselayer import mail
+
+class CommonPTCLayer(ptc.BasePTCLayer):
+    """A layer with some common useful testing and debugging setup."""
+
+    def afterSetUp(self):
+        """Apply some useful testing and debugging changes"""
+
+        # Don't ignore exceptions so that problems don't hide behind
+        # Unauthorized or NotFound exceptions when doing functional
+        # testing.
+        error_props = self.portal.error_log.getProperties()
+        error_props['ignored_exceptions'] = ('Redirect',)
+        error_props = self.portal.error_log.setProperties(
+            **error_props)
+
+        # Put resource registries in debug mode to make it easier to
+        # inspect CSS, JavaScript, and KSS
+        self.portal.portal_css.setDebugMode(True)
+        self.portal.portal_javascripts.setDebugMode(True)
+        self.portal.portal_kss.setDebugMode(True)
+
+common_layer = CommonPTCLayer([mail.mockmailhost_layer])
