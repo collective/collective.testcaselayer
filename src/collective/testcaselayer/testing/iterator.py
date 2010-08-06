2dc2626f788b5beb3ac30b939e65f4c9c54cf2b3
rossp <rossp@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-06 22:37:42 +0000

    Fix Plone 4 compatibility

--- src/collective/testcaselayer/testing/iterator.py
+++ src/collective/testcaselayer/testing/iterator.py
@@ -1,4 +1,11 @@
+from zope.interface import interfaces
+from zope import interface
+
 from ZPublisher.Iterators import IStreamIterator
 
+
 class StreamIterator(list):
-    __implements__ = (IStreamIterator,)
+    if interfaces.IInterface.providedBy(IStreamIterator):
+        interface.implements(IStreamIterator)
+    else:
+        __implements__ = (IStreamIterator, )  # BBB Plone 3
